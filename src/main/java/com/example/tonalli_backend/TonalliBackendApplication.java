df4a38a4ef421642adb758c840e3d9223ad750ca
Paola Martinez <quintaspaola22@gmail.com>
2025-11-11 18:03:08 -0600

    inital commit

---
+++ src/main/java/com/example/tonalli_backend/TonalliBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.tonalli_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TonalliBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TonalliBackendApplication.class, args);
+	}
+
+}
